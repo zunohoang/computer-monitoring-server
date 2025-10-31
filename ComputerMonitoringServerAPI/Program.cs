c87a18390a9db56864ef6a474672eac5648dc5ba
Nguyen Van Hoang <nguyenvanhoang2005nt@gmail.com>
2025-11-01 01:19:59 +0700

    fix: disable redicect http -> https

--- ComputerMonitoringServerAPI/Program.cs
+++ ComputerMonitoringServerAPI/Program.cs
@@ -108,7 +108,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 app.UseCors("AllowAll");
 
 app.UseAuthentication();
